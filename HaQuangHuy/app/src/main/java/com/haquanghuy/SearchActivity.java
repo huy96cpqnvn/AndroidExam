a4df08d1f87e8de6ba42c8711424840c63680f2d
huy96cpqnvn <huy96cpqnvn@gmail.com>
2021-03-18 21:15:05 +0700

    update search

--- HaQuangHuy/app/src/main/java/com/haquanghuy/SearchActivity.java
+++ HaQuangHuy/app/src/main/java/com/haquanghuy/SearchActivity.java
@@ -1,12 +1,14 @@
 package com.haquanghuy;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class SearchActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
